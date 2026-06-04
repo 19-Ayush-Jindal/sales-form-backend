b96e7b6c423a237fd2375095ae63f627831ab0e9
19-Ayush-Jindal <19.a.jindal@gmail.com>
2026-06-04 22:56:13 +0530

    Render deployment setup

--- googlesheets.js
+++ googlesheets.js
@@ -1,7 +1,9 @@
 import { google } from "googleapis";
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: "./backend/credentials.json",
+    credentials: JSON.parse(
+        process.env.GOOGLE_CREDENTIALS
+    ),
     scopes: [
         "https://www.googleapis.com/auth/spreadsheets"
     ]
